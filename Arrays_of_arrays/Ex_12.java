87abb308e11c8fdfacfd021409834e86b8fbc343
Ruslan2322 <62638170+Ruslan2322@users.noreply.github.com>
2020-04-27 15:05:22 +0300

    Ex_12

---
+++ Arrays_of_arrays/Ex_12.java
@@ -0,0 +1,74 @@
+package Lesson02_Arrays_of_arrays;
+
+//Отсортировать строки матрицы по возрастанию и убыванию значений.
+
+public class Ex_12 {
+
+    private static int[][] mass;
+
+    public static void main(String[] args) {
+        int n = 5;
+        int quant;
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = (int) (Math.random() * 100);
+            }
+        }
+        System.out.println("Стартовая матрица");
+        Arrprint(arr);
+        for (int i = 0; i < arr.length; i++) {
+            quant = 0;
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[i][j + 1] < arr[i][j]) {
+                    int A = arr[i][j];
+                    arr[i][j] = arr[i][j + 1];
+                    arr[i][j + 1] = A;
+                    quant++;
+                }
+            }
+            if (quant != 0)
+                i--;
+        }
+        System.out.println("По возрастанию");
+        Arrprint(arr);
+        for (int i = 0; i < arr.length; i++) {
+            quant = 0;
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[i][j + 1] > arr[i][j]) {
+                    int A = arr[i][j];
+                    arr[i][j] = arr[i][j + 1];
+                    arr[i][j + 1] = A;
+                    quant++;
+                }
+            }
+            if (quant != 0)
+                i--;
+        }
+        System.out.println("По убыванию");
+                Arrprint(arr);
+        for (int i = 0; i < arr.length; i++) {
+            quant = 0;
+            for (int j = 0; j < arr[i].length - 1; j++) {
+                if (arr[i][j - 1] > arr[i][j]) {
+                    int A = arr[i][j];
+                    arr[i][j] = arr[i][j + 1];
+                    arr[i][j + 1] = A;
+                    quant++;
+                }
+            }
+            if (quant != 0)
+                i--;
+        }
+    }
+        public static void Arrprint ( int[][] arr){
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    System.out.print(arr[i][j] + "\t");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+
